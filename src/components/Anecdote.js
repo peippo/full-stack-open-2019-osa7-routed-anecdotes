a413a679e77cb49de6298f7ae3ad2319fae9ac9a
Janne Peippo <peippo@lumous.com>
2019-11-07 18:46:12 +0200

    Add osa 7a 7.2 answer

---
+++ src/components/Anecdote.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Anecdote = ({ anecdote }) => {
+	return (
+		<div>
+			<h2>{anecdote.content}</h2>
+			<p>Has {anecdote.votes} votes</p>
+			<p>
+				For more info see: <a href="{anecdote.info}">{anecdote.info}</a>
+			</p>
+		</div>
+	);
+};
+
+export default Anecdote;
---
+++ src/components/AnecdoteList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const AnecdoteList = ({ anecdotes }) => {
+	return (
+		<div>
+			<h2>Anecdotes</h2>
+			<ul>
+				{anecdotes.map(anecdote => (
+					<Link to={`/anecdotes/${anecdote.id}`} key={anecdote.id}>
+						<li>{anecdote.content}</li>
+					</Link>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export default AnecdoteList;
